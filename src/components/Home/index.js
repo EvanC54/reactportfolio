c18a8cb4b9f2ba9fd7da0ca20697771559af69b1
Evan C <evancummings3@gmail.com>
2023-03-13 18:25:28 -0500

    '#' 1st

---
+++ src/components/Home/index.js
@@ -0,0 +1,48 @@
+import { Link } from 'react-router-dom';
+import logoe from '../../assets/images/logoe.png'
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Sidebar from '../Sidebar';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['v', 'a', 'n']
+    const jobArray = ['W', 'e', 'b', ' ', 'D', 'e', 'v', 'e', 'l', 'o','p', 'e', 'r']
+
+    useEffect(() => {
+          setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 4000)
+      }, [])
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                <span className={letterClass}> H</span>
+                <span className={`${letterClass}_12`} >i, </span>
+                <br/> 
+                <span className={`${letterClass}_13`} >I</span>
+                <span className={`${letterClass}_14`} >'m </span>
+                <img src={logoe} alt='developer'/>
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={nameArray} idx={15}/>
+                <br/>
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={jobArray} idx={22}/>
+                </h1>
+                <h2> Full-Stack Developer | React Conesuer | Day Trader </h2>
+                <Link to='/contact' className='flat-button'> CONTACT ME </Link>
+            </div>
+
+        </div>
+
+
+    );
+
+
+}
+
+export default Home 
